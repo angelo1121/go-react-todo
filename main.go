32afc083e2bdd55da8fdd558d55f9e62d8dad63c
angelo <uns.248@gmail.com>
2024-06-07 02:51:13 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Body      string `json:"body"`
+	Completed bool   `json:"completed"`
+}
+
+func main() {
+	app := fiber.New()
+
+	if err := godotenv.Load('.env'); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	todos := []Todo{}
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := Todo{}
+		if err := c.BodyParser(&todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required."})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, todo)
+
+		return c.Status(fiber.StatusCreated).JSON(todo)
+	})
+
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if strconv.Itoa(todo.ID) == id {
+				todos[i].Completed = !todos[i].Completed
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found."})
+	})
+
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if strconv.Itoa(todo.ID) == id {
+				todos = RemoveIndex(todos, i)
+				return c.Status(200).JSON(fiber.Map{"success": true})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found."})
+	})
+
+	log.Fatal(app.Listen(":"+os.Getenv("SERVER_PORT")))
+}
+
+// RemoveIndex removes the element at index i from a slice while maintaining order.
+func RemoveIndex[T any](s []T, i int) []T {
+	return append(s[:i], s[i+1:]...)
+}
